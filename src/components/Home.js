7a72d2e2696d5c722caff2434df5a18efa625621
ketchanji <valerydjanya8@gmail.com>
2023-02-27 21:40:01 +0100

    left with the product page

--- src/components/Home.js
+++ src/components/Home.js
@@ -40,7 +40,7 @@ function Home() {
                 class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-200 dark:border-gray-200 dark:placeholder-gray-800 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
                 type="text"
                 required
-                placeholder="Create Room Code"
+                placeholder="Create Class"
                 aria-label="Email Address"
                 value={RoomCode}
                 onChange={(e) => setRoomCode(e.target.value)}
@@ -49,7 +49,7 @@ function Home() {
 
             <div class="flex items-center justify-between mt-4">
               <button class="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                Create Room
+                Create Class Room
               </button>
             </div>
           </form>
