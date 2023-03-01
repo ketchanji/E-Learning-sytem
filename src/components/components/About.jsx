25aaa709970eedcaeec2a813ebe3a56dc1101dea
ketchanji <valerydjanya8@gmail.com>
2023-03-01 09:37:57 +0100

    left with the product page

--- src/components/components/About.jsx
+++ src/components/components/About.jsx
@@ -108,7 +108,9 @@ export const About = () => {
         </div>
       </div>
 
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20" data-aos="flip-up" data-aos-duration="1000">
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20" data-aos="flip-right"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="1000">
         <div className="grid gap-24 row-gap-8 lg:grid-cols-5">
           <div className="grid gap-8 lg:col-span-2">
             <div>
